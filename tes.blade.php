b1ad03edd13be95587f5bda01f3cdb0ef2389ddd
filzarr <filzaramadhan2003@students.usu.ac.id>
2024-05-13 21:01:08 +0700

    css

--- tes.blade.php
+++ tes.blade.php
@@ -1,35 +1,22 @@
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:cover {"url":"http://ptpn.test/wp-content/uploads/2024/05/bg-bayuwangi-scaled.jpg","id":8,"dimRatio":50,"customOverlayColor":"#9cacb0","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"top center","isDark":false,"align":"wide","style":{"color":{"duotone":"var:preset|duotone|foreground-and-background"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignwide is-light has-custom-content-position is-position-top-center" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#9cacb0"></span><img class="wp-block-cover__image-background wp-image-8" alt="" src="http://ptpn.test/wp-content/uploads/2024/05/bg-bayuwangi-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
-<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"metadata":{"name":"navb"},"align":"wide","style":{"spacing":{"padding":{"bottom":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dspacing\u002d\u002dlarge, 8rem)","top":"var(\u002d\u002dwp\u002d\u002dcustom\u002d\u002dspacing\u002d\u002dsmall, 1.25rem)"}}},"layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"center"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--small, 1.25rem);padding-bottom:var(--wp--custom--spacing--large, 8rem)"><!-- wp:group {"layout":{"type":"flex"}} -->
-<div class="wp-block-group"><!-- wp:site-logo {"width":64} /-->
-
-<!-- wp:site-title /--></div>
-<!-- /wp:group -->
-
-<!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"stretch"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","className":"is-style-fill"} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-text-align-center wp-element-button" href="http://sistem-informai-ptpn.test">Login</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"tagName":"main"} -->
-<main class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><strong>Sistem Informasi Pengolahan cpo(crude palm oil)</strong> <strong>pt. perkebunan nusantara iv gunung bayu</strong></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://sistem-informai-ptpn.test">Get Started</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></main>
-<!-- /wp:group --></div></div>
-<!-- /wp:cover --></div>
-<!-- /wp:group -->
+.wp-site-blocks{
+    padding-left:0 !important;
+    padding-right:0 !important;
+    }
+    body { 
+        --wp--style--global--wide-size: 100% !important;
+    }
+    .main-navigation {
+        width: 100%; 
+    padding-top:0 !important; 
+    padding-bottom:0 !important; 
+    padding-left:1em;
+    padding-right:1em
+    } 
+    .wp-block-cover{
+    padding-top:0 !important; }
+    .wp-block-group{
+    padding-top:0 !important;}
+    body .is-layout-constrained > :where(:not(.alignleft):not(.alignright):not(.alignfull)){
+    max-width:100%;
+    padding-top:16px !important
+    }
